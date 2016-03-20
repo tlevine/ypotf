9a8e14301872de99389de205bbf76f95ae9176e5
Thomas Levine <_@thomaslevine.com>
2016-03-20 14:25:11 +0000

    categorize emails

---
+++ ypotf/subject.py
@@ -0,0 +1,15 @@
+import re
+
+MATCHERS = [(k: re.compile(v, flags=re.IGNORECASE)) for k,v in [
+    'subscribe': r'^subscribe$',
+    'unsubscribe': r'^unsubscribe$',
+    'confirm': r'list-confirm-[a-z0-9]{32}',
+    'archive': r'^list-archive'
+    'help': r'help',
+]]
+
+def categorize(subject):
+    for cat, expr in MATCHERS:
+        if re.match(expr, subject):
+            return cat
+    return 'message'
