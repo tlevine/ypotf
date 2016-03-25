85612fd3f8de0125a667a020164dc262b1eacf3b
Thomas Levine <_@thomaslevine.com>
2016-03-25 18:28:42 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='ypotf',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Mailing list software that runs as an IMAP and SMTP client.',
+      url='http://dada.pink/ypotf/',
+      packages=['ypotf'],
+      install_requires = [
+      ],
+      tests_require = [
+          'pytest>=2.6.4',
+      ],
+      version='0.0.0',
+      license='AGPL',
+      entry_points = {'console_scripts': ['ypotf = ypotf.ypotf']},
+)
