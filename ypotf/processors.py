92ca905c403ac85928243342038cf22cd23eedd8
Thomas Levine <_@thomaslevine.com>
2016-03-26 21:09:58 +0000

    Automatic commit with j

--- ypotf/processors.py
+++ ypotf/processors.py
@@ -22,10 +22,13 @@ def process(M, m):
     confirmations = storage.Confirmations(M)
     subscribers = storage.Subscribers(M)
     if re.match(MATCHERS['subscriptions'], m['subject']):
-        return add_confirmation(confirmations, m['message-id'],
-                                m['subject'], m['from'])
-    elif re.match(MATCHERS['help'], subject):
-        return templates.help(date = m['date'])
+        code = _confirmation_code()
+        confirmations[code] = '%s %s' % (m['subject'], m['from'])
+        return templates.confirmation(
+            references=m['message-id'],
+            subject='Re: Your %s request' % m['subject'].strip().lower()
+            confirmation_code=code,
+        )
     elif re.match(MATCHERS['confirmations'], subject)
         code = re.match(MATCHERS['confirmations'], subject).group(1)
         action, argument = confirmations[code].partition(' ')
@@ -37,11 +40,16 @@ def process(M, m):
             del(subscribers[argument])
         else:
             raise ValueError
+    elif re.match(MATCHERS['help'], subject):
+        return templates.help(date = m['date'])
     else:
-        code = bytes(random.randint(32, 126) for _ in range(32))
+        code = _confirmation_code()
         confirmations[code] = '%s %s' % ('message', m['message-id'])
         return templates.confirmation(
             references=m['message-id'],
             subject='Re: ' + m['subject'].strip()
             confirmation_code=code,
         )
+        
+def _confirmation_code():
+    return bytes(random.randint(32, 126) for _ in range(32))
