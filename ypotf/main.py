92c497d0bd3d9af86b6144ccccd09b08ad849213
Thomas Levine <_@thomaslevine.com>
2016-03-26 15:57:45 +0000

    integrating processors

--- ypotf/main.py
+++ ypotf/main.py
@@ -1,6 +1,11 @@
 import imaplib
 
+from . import processors
+
 def ypotf(host:str, address:str, password:str):
     M = imaplib.IMAP4_SSL(host)
     M.login(address, password)
+    M.select('INBOX')
+    for num, m in processors.messages(M):
+        u
     M.logout()
--- ypotf/processors.py
+++ ypotf/processors.py
@@ -32,7 +32,7 @@ def unsubscribe(M, num_i):
     email_address = _message(num_i)['from']
     M.close()
     M.select('ypotf-list')
-    for num_j, m in _messages();
+    for num_j, m in messages(M);
         if m['subject'] == email_address:
             M.store(num_j, '+FLAGS', '\\Deleted')
             M.expunge()
@@ -45,18 +45,21 @@ def unsubscribe(M, num_i):
 
 def send_message(M, message_id):
     M.select('ypotf-queue')
-    for num, msg in _messages(M):
+    for num, msg in messages(M):
         if msg['message-id'] == message_id:
             raise NotImplementedError('Send the message with SMTP')
             M.copy(num, 'Sent')
-    M.expunge()
+            M.expunge()
+            break
+    else:
+        raise ValueError('No such message in the queue')
     M.close()
 
 def queue_message(M, num):
     M.copy(num, 'ypotf-queue')
     M.expunge()
 
-def _messages(M):
+def messages(M):
     typ, data = M.search(None, 'ALL')
     nums = data[0].split()
 
