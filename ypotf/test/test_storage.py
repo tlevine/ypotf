fcaeb3440e2a5a11679a0b4f4a4c0f7b00c66018
Thomas Levine <_@thomaslevine.com>
2016-03-30 17:49:46 +0000

    storage testing

--- ypotf/test/test_storage.py
+++ ypotf/test/test_storage.py
@@ -8,16 +8,21 @@ def _now():
     return tuple(datetime.datetime.now().timetuple())
 
 def test_blah(imap):
-    m = Message()
     imap.select('INBOX')
 
-    m['Subject'] = 'unsubscribe'
-    imap.append('INBOX', None, _now(), m.as_bytes())
+    m1 = Message()
+    m1['Subject'] = 'unsubscribe'
+    imap.append('INBOX', None, _now(), m1.as_bytes())
 
-    m['Subject'] = 'subscribe'
-    imap.append('INBOX', None, _now(), m.as_bytes())
+    m2 = Message()
+    m2['Subject'] = 'subscribe'
+    imap.append('INBOX', None, _now(), m2.as_bytes())
 
     imap.close()
 
     imap.select('INBOX')
     assert storage.message_nums(imap) == [b'1', b'2']
+
+    num, m = storage.first_message(imap)
+    assert num == b'1'
+    assert m.as_string() == m1.as_string()
