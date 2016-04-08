33816cb3c905c718014e5c08260381504a5bc943
Thomas Levine <_@thomaslevine.com>
2016-04-08 21:23:30 +0000

    refactor transaction store

--- ypotf/process.py
+++ ypotf/process.py
@@ -26,9 +26,6 @@ CHARACTERS = string.ascii_lowercase + string.digits
 def _confirmation_code():
     return ''.join(sample(CHARACTERS, 32))
 
-def _store(M, num, action, flags):
-    return r(M.store(num, action, flags))
-
 def _append(box, M, flags, m):
     d = tuple(datetime.datetime.now().timetuple())
     if 'seen' not in flags.lower():
@@ -46,16 +43,34 @@ def _message(**headers):
     return m  
 
 class Transaction(object):
+    def __init__(self, M):
+        self.M = M
+
     def __enter__(self):
-        self._queue = []
-        return self._queue
+        self._finalize = []
+        self._revert = []
+        return self._finalize
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_type is not None:
-            logger.info('Exception occurred in transaction, aborting.')
-        else:
-            for f, args in self._queue:
+        if exc_type is None:
+            for f, args in self._finalize:
                 f(*args)
+        else:
+            logger.info('Exception occurred in transaction, aborting.')
+            for f, args in self._revert:
+                f(*args)
+
+    def plus_flags(self, num, flags):
+        self._revert.append((self._store, (num, '-FLAGS', flags)))
+        return self._store(num, '+FLAGS', flags)
+
+    def minus_flags(self, num, *flags):
+        self._revert.append((self._store, (num, '+FLAGS', flags)))
+        return self._store(num, '-FLAGS', flags)
+
+    def _store(self, num, action, flags):
+        return r(M.store(num, action, flags))
+
 
 FORWARDED_HEADERS = {
     'from', 'to',
@@ -132,8 +147,8 @@ def process(S, M, num, from_address, subject, message_id):
                 (send, (templates.confirmation_message(action, code),
                         from_address)),
                 (_append, ('Inbox', M, '\\SEEN \\DRAFT', m)),
-                (_store, (M, num, '+FLAGS', '\\SEEN \\ANSWERED'))
             ])
+            t.plus_flags(num, '\\SEEN \\ANSWERED')
 
     elif action == 'subscribe':
         flags = '\\FLAGGED \\DRAFT \\SEEN'
@@ -160,15 +175,15 @@ def process(S, M, num, from_address, subject, message_id):
             if draft_num and code:
                 # Add confirmation code to unsubscribe message.
                 t.extend([
-                    (_store, (M, draft_num, '+FLAGS', '\\DELETED')),
                     (_append, ('Inbox', M, '\\FLAGGED \\SEEN',
                                _message(to=code, subject=from_address))),
                     (send, (templates.confirmation_message(action, code),
                             from_address)),
                 ])
+                t.plus_flags(draft_num, '\\DELETED')
             else:
                 t.append(send, (templates.not_a_member(), from_address))
-            t.append(_store, (M, num, '+FLAGS', '\\SEEN \\ANSWERED'))
+            t.plus_flags(num, '\\SEEN \\ANSWERED'))
 
     elif action == 'list-confirm':
         code = re.match(MATCHERS['list-confirm'], subject).group(1)
