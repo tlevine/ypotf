4f1f6bb5acc97989b114e284ac9252bec11c1d56
Thomas Levine <_@thomaslevine.com>
2016-04-08 04:38:09 +0000

    Automatic commit with j

--- ypotf/process.py
+++ ypotf/process.py
@@ -37,47 +37,11 @@ def process(M, num, from_address, subject, message_id):
     send = partial(full_send, message_id)
 
     if action == 'help':
-        raise NotImplementedError
+        send(_help(), from_address)
+        r(M.store(num, '+FLAGS', '\\SEEN \\ANSWERED'))
+
     elif action == 'list-archive'
         raise NotImplementedError
-
-    elif action == 'subscribe':
-        FLAGS = '\\FLAGGED \\DRAFT \\SEEN'
-        draft_num, code = subscriber(M, from_address)
-        if draft_num and code:
-            # Reuse the existing message.
-            M.store(draft_num, '+Flags', flags)
-        else:
-            code = _confirmation_code()
-            _append(M, flags, _message(to=code, subject=from_address))
-        send(_confirmation_message(from_address, action, code))
-
-    elif action == 'unsubscribe':
-        FLAGS = '\\FLAGGED \\SEEN'
-        draft_num, code = subscriber(M, from_address)
-        if draft_num and code:
-            M.store(draft_num, '+FLAGS', '\\DELETED')
-            _append(M, flags, _message(to=code, subject=from_address))
-            send(_confirmation_message(from_address, action, code))
-        else:
-            send(_not_a_member(from_address)
-
-    elif action == 'list-confirm':
-        code = re.match(MATCHERS['list-confirm'], subject).group(1)
-        c_num, c_action = searches.Inbox.confirmations(M, code)
-        if c_num and c_action:
-            if c_action == 'message':
-                data = r(M.fetch(num, '(RFC822)'))
-                send(message_from_bytes(data[0][1]))
-
-                M.copy(c_num, 'Sent')
-                M.store(c_num, '+FLAGS', '\\DELETED')
-            elif c_action == 'subscribe':
-                M.store(c_num, '-FLAGS', '\\DRAFT')
-            elif c_action == 'unsubscribe':
-                M.store(c_num, '-FLAGS', '\\FLAGGED')
-            else:
-                raise ValueError
 
     elif action == 'message':
         code = _confirmation_code()
@@ -86,14 +50,67 @@ def process(M, num, from_address, subject, message_id):
         m = message_from_bytes(data[0][1])
         m['TO'] = code
 
+
+        send(_confirmation_message(action, code), from_address)
         _append(M, '\\SEEN \\DRAFT', m)
+        r(M.store(num, '+FLAGS', '\\SEEN \\ANSWERED'))
+
+    elif action == 'subscribe':
+        FLAGS = '\\FLAGGED \\DRAFT \\SEEN'
+        draft_num, code = subscriber(M, from_address)
+
+        if draft_num and code:
+            # Reuse the existing message.
+            M.store(draft_num, '+Flags', flags)
+        else:
+            code = _confirmation_code()
+            _append(M, flags, _message(to=code, subject=from_address))
+
+        send(_confirmation_message(action, code), from_address)
+        r(M.store(num, '+FLAGS', '\\SEEN \\ANSWERED'))
+
+    elif action == 'unsubscribe':
+        FLAGS = '\\FLAGGED \\SEEN'
+        draft_num, code = subscriber(M, from_address)
+
+        if draft_num and code:
+            M.store(draft_num, '+FLAGS', '\\DELETED')
+            _append(M, flags, _message(to=code, subject=from_address))
+            send(_confirmation_message(action, code), from_address)
+        else:
+            send(_not_a_member(), from_address)
+
+        r(M.store(num, '+FLAGS', '\\SEEN \\ANSWERED'))
+
+    elif action == 'list-confirm':
+        code = re.match(MATCHERS['list-confirm'], subject).group(1)
+        c_num, c_action = searches.Inbox.confirmations(M, code)
+        if c_num and c_action:
+            if c_action == 'message':
+                data = r(M.fetch(num, '(RFC822)'))
+
+                send(message_from_bytes(data[0][1]), None)
+                r(M.copy(c_num, 'Sent'))
+                r(M.store(c_num, '+FLAGS', '\\DELETED'))
+
+                r(M.store(num, '+FLAGS', '\\SEEN \\ANSWERED'))
+
+            elif c_action == 'subscribe':
+                r(M.store(c_num, '-FLAGS', '\\DRAFT'))
+                r(M.store(num, '+FLAGS', '\\SEEN'))
+
+            elif c_action == 'unsubscribe':
+                r(M.store(c_num, '-FLAGS', '\\FLAGGED'))
+                r(M.store(num, '+FLAGS', '\\SEEN'))
+
+            else:
+                raise ValueError
 
     else:
         raise ValueError('Bad action')
 
 
 
-    r(M.store(num, '+FLAGS', '\\Seen')
     return template.configure(
         'sender',
         to_address=m['From'],
