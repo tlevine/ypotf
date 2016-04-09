435fffa731d857dd933c3c08e44afc25385c0c79
Thomas Levine <_@thomaslevine.com>
2016-04-09 20:51:26 +0000

    pager

--- ypotf/interactive.py
+++ ypotf/interactive.py
@@ -1,4 +1,5 @@
 import subprocess, os
+
 def subscribe(m):
     '''
     '''
@@ -6,5 +7,8 @@ def subscribe(m):
 
 
 def _pager(text):
-    sp = subprocess.Popen([os.environ["EDITOR"]])
+    sp = subprocess.Popen(os.environ["PAGER"],
+        stdin=subprocess.PIPE, shell=True)
+    sp.stdin.write(text.encode('utf-8'))
+    sp.stdin.close()
     sp.wait()
