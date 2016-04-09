395ff08392a64d426daa1fbc5d871d1bc60f2383
Thomas Levine <_@thomaslevine.com>
2016-04-09 15:01:29 +0000

    Automatic commit with j

--- ypotf/templates.py
+++ ypotf/templates.py
@@ -1,34 +1,36 @@
 from email.message import Message
 from functools import partial
 
-list_address = '_@dada.pink'
 FORWARDED_HEADERS = {
     'from',
     'subject', 'date', 'user-agent',
     'mime-version', 'content-type', 'content-transfer-encoding',
     'message-id', 'in-reply-to', 'references',
 }
-LIST_HEADERS = {
-    'List-Id': list_address.replace('@', '.'),
-    'List-Unsubscribe': 'mailto:%s?subject=unsubscribe' % list_address,
-    'List-Archive': 'mailto:%s?subject=list-archive' % list_address,
-    'List-Post': 'mailto:%s' % list_address,
-    'List-Help': 'mailto:%s?subject=help' % list_address,
-    'List-Subscribe': 'mailto:%s?subject=subscribe' % list_address,
-}
-def _set_list_headers(msg):
+def _list_headers(list_address):
+    return {
+        'List-Id': list_address.replace('@', '.'),
+        'List-Unsubscribe': 'mailto:%s?subject=unsubscribe' % list_address,
+        'List-Archive': 'mailto:%s?subject=list-archive' % list_address,
+        'List-Post': 'mailto:%s' % list_address,
+        'List-Help': 'mailto:%s?subject=help' % list_address,
+        'List-Subscribe': 'mailto:%s?subject=subscribe' % list_address,
+    }
+
+def _set_list_headers(list_address, msg):
+    list_headers = _list_headers(list_address)
     for header in msg:
         if header.lower() not in FORWARDED_HEADERS:
             del(msg[header])
 
-    for k, v in LIST_HEADERS.items():
+    for k, v in list_headers.items():
         if k in msg:
             del(msg[k])
         msg[k] = v
     return msg
 
-def _confirmation(action, to_address, code):
-    m = _set_list_headers(Message())
+def _confirmation(action, list_address, to_address, code):
+    m = _set_list_headers(list_address, Message())
     m['To'] = to_address
     m['From'] = list_address
     m['Subject'] = 'Verify your email address {%s}' % code
@@ -41,34 +43,32 @@ def _confirmation(action, to_address, code):
     m.set_payload(tpl % _desc[action])
     return m
 
-subscribe = partial(_confirmation, 'subscribe')
-unsubscribe = partial(_confirmation, 'unsubscribe')
-message_confirm = partial(_confirmation, 'message')
+class new(object):
+    subscribe = partial(_confirmation, 'subscribe')
+    unsubscribe = partial(_confirmation, 'unsubscribe')
+    @staticmethod
+    def help(list_address, to_address):
+        m = _set_list_headers(list_address, Message())
+        m['To'] = to_address
+        m['From'] = list_address
+        m['Subject'] = 'List help'
+        m['Date'] = email.utils.format_datetime(_now())
+        m.set_payload('Documentation will eventually go here.')
+        return m
 
-def help(to_address):
-    m = _set_list_headers(Message())
-    m['To'] = to_address
-    m['From'] = list_address
-    m['Subject'] = 'List help'
-    m.set_payload('Documentation will eventually go here.')
-    return m
+class envelope(object):
+    @staticmethod
+    def publication(list_address, msg):
+        m = _set_list_headers(list_address, msg)
+        del(msg['To'])
+        msg['To'] = list_address
+        m['X-Ypotf-Id'] = _uuid()
+        m['X-Ypotf-Date'] = email.utils.format_datetime(_now())
+        return msg
 
-def message_send(msg):
-    m = _set_list_headers(msg)
-    del(msg['To'])
-    msg['To'] = list_address
-    return msg
-
-# Internal emails
-def envelope(**headers):
-    m = Message()
-    for key, value in headers.items():
-        m[key] = value
-    return m
-
-def i_subscriber(address, code):
-    return i_message_confirmation(envelope(Subject=address), code)
-
-def i_message_confirmation(m, code):
-    m['X-Ypotf-Confirmation'] = code
-    return m
+    @staticmethod
+    def subscriber(address):
+        m = _set_ypotf_headers(Message())
+        m['Subject'] = address
+        m['X-Ypotf-Id'] = _uuid()
+        return m
