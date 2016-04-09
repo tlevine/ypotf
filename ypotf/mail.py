f31f762b92c81656ed773c8964698ff4e688f637
Thomas Levine <_@thomaslevine.com>
2016-04-09 14:02:05 +0000

    better name

--- ypotf/mail.py
+++ ypotf/mail.py
@@ -9,7 +9,7 @@ log_tpl = '''%s this message to %s
 %s
 ----------------------------------------''' 
 
-class DB(object):
+class Mail(object):
     def __init__(self, S, M, list_address):
         self._S = S
         self._M = M
@@ -47,15 +47,15 @@ class DB(object):
         return self.store(num, '+FLAGS', '\\DELETED')
 
     def append_applied(self, m):
-        return self.append('Inbox', '\\ANSWERED \\SEEN', m)
+        return self._append('Inbox', '\\ANSWERED \\SEEN', m)
 
     def append_pending(self, m):
-        return self.append('Inbox', '\\SEEN', m)
+        return self._append('Inbox', '\\SEEN', m)
 
     def append_sent(self, m):
-        return self.append('Sent', '\\SEEN', m)
+        return self._append('Sent', '\\SEEN', m)
 
-    def store(self, num, action, flags):
+    def _store(self, num, action, flags):
         actions = {
             '+FLAGS': '-FLAGS',
             '-FLAGS': '+FLAGS',
@@ -63,32 +63,18 @@ class DB(object):
         if action in actions:
             anti_action = actions[action]
             self._revert.append((self._store, (num, anti_action, flags)))
-            return self._store(num, action, flags)
+            return self._revert_store(num, action, flags)
         else:
             raise ValueError('Bad action: %s' % action)
 
-    def _store(self, *args):
+    def _revert_store(self, *args):
         '''
         :param args: Tuple of num, action, flags
         '''
         logger.debug('STORE %d %s (%s)' % *args)
         return r(self._M.store(*args))
 
-    def send(self, msg, *to_addresses):
-        logger.info('Sending to %d addresses' % len(to_addresses))
-        msg2 = _prepare_send(msg) # sent message kind 2
-
-        if msg2['To'].lower() == self._list_address:
-            msg2['Bcc'] = ', '.join(to_addresses)
-            self.append('Sent', '\\SEEN', msg2)
-
-        for to_address in to_addresses:
-            msg1 = _prepare_send(msg) # sent message kind 1
-            logger.debug(log_tpl % ('Sending', to_address, msg1))
-            self.append('Sent', '\\SEEN', msg1)
-            self.S.send_message(msg1, self._list_address, [to_address])
-
-    def append(self, box, flags, m):
+    def _append(self, box, flags, m):
         '''
         It is possible to append to boxes other than the current one.
         This gets interesting.
@@ -114,3 +100,16 @@ class DB(object):
         num = search.get_num(M, query)
         self._store(num, '+FLAGS', '\\DELETED')
 
+    def send(self, msg, *to_addresses):
+        logger.info('Sending to %d addresses' % len(to_addresses))
+        msg2 = _prepare_send(msg) # sent message kind 2
+
+        if msg2['To'].lower() == self._list_address:
+            msg2['Bcc'] = ', '.join(to_addresses)
+            self._append('Sent', '\\SEEN', msg2)
+
+        for to_address in to_addresses:
+            msg1 = _prepare_send(msg) # sent message kind 1
+            logger.debug(log_tpl % ('Sending', to_address, msg1))
+            self._append('Sent', '\\SEEN', msg1)
+            self.S.send_message(msg1, self._list_address, [to_address])
